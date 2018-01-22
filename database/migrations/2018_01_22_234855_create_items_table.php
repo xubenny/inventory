4db85274c4ae9b42252c46399438ace7148357ac
xubenny <xubinglin@hotmail.com>
2018-01-22 15:54:28 -0800

    added items table

--- database/migrations/2018_01_22_234855_create_items_table.php
+++ database/migrations/2018_01_22_234855_create_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('vendor');
+            $table->string('model');
+            $table->string('memo');
+            $table->string('status');
+            $table->string('user');
+            $table->string('location');
+            $table->integer('quantity');
+            $table->dateTime('purchased_at');
             $table->timestamps();
         });
     }
@@ -30,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('items');
     }
 }
