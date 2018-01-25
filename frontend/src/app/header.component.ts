2c30e6d369612309c1ba70f03738cc629bf3723c
xubenny <xubinglin@hotmail.com>
2018-01-25 15:13:01 -0800

    add add item

--- frontend/src/app/header.component.ts
+++ frontend/src/app/header.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component ({
     selector: 'app-header',
     templateUrl: 'header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    addItem() {
+    }
+}
